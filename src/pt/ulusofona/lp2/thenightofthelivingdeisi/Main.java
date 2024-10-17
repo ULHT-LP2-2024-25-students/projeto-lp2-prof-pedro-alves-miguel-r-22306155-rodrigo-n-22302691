fafbcff8fbe79064eceb5e84203d9b835897d77c
ThatPortugueseGuy <rodrigon20155@gmail.com>
2024-10-17 18:44:40 +0100

    Launcher

--- src/pt/ulusofona/lp2/thenightofthelivingdeisi/Main.java
+++ src/pt/ulusofona/lp2/thenightofthelivingdeisi/Main.java
@@ -1,9 +1,11 @@
 package pt.ulusofona.lp2.thenightofthelivingdeisi;
+import pt.ulusofona.lp2.guiSimulator.*;
 
 
 public class Main {
 
     public static void main(String[] args) {
-
+        AppLauncher launcher = new AppLauncher();
+        AppLauncher.main(null);
     }
 }
